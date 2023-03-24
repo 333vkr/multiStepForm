bb3600193082c044fa749a60db1d460fc4761554
vaishnav k <333vkr@gmail.com>
2023-03-24 22:35:34 +0530

    main card design

---
+++ src/Components/MainCard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../css/mainCard.css'
+const MainCard = () => {
+  return (
+    <div className='main-card-container'>
+    <div className='main-card-left'>left container</div>
+    <div className='main-card-right'>right container</div>
+    </div>
+
+  )
+}
+
+export default MainCard
